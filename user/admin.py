8f72860b460483705e1db88012b4e38d10bea6a4
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-26 14:40:59 +0200

    Register custom user to admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CreationForm, ChangeForm
+from .models import User
 
-# Register your models here.
+
+class CustomAdmin(UserAdmin):
+    add_form = CreationForm
+    form = ChangeForm
+    model = User
+    list_display = ("email","first_name", "last_name", "is_staff", "is_active",
+                    "is_superuser")
+    list_filter = ("last_name", "email", "is_staff", "is_active",)
+    fieldsets = (
+        ("Userdata", {"fields": ("image", "email", "first_name", "last_name",
+                                 "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups",
+                                    "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomAdmin)
