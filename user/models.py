1199732b595a7d17981f444923ad5fa4225ffcb8
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-26 14:41:39 +0200

    reate custom user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    The User class is a custom class written for the project.
+    """
+    image = CloudinaryField()
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_changed = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField()
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
