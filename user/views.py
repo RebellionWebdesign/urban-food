045176139c83779087d66ae54a36efe23ce0db46
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 18:33:31 +0200

    Add user view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
+from typing import Any
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+class BaseView(DetailView):
+
+    def get(self, request):
+        return render(request, 'base.html')
+
+class UserProfile(LoginRequiredMixin, DetailView):
+    template_name = 'user/profile.html'
+    context_object_name = 'user_profile'
+
+    def get(self, request, **kwargs):
+        users = User.objects.all()
+        context = {
+            "users" : users,
+        }
+
+        return render(request, 'user/profile.html', context)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = context['user_profile'].filter(user=self.request.user)
+
+        return context
