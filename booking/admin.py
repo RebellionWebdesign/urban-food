afbb0581b41635916ff17ae1870cb049caada124
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 10:23:41 +0200

    Register booking to admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('number', 'last_name', 'email')
+    list_display = ['number', 'last_name', 'email', 'table', 'persons', 'date',
+                    'time']
+    search_fields = ['number', 'last_name', 'email', 'table', 'persons',
+                     'date']
