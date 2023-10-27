36f3d72fca2f139ea313f7a90162bbcf0715d94e
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 09:44:27 +0200

    Create table model

--- table/models.py
+++ table/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class table(models.Model):
+    number = models.IntegerField()
+    persons = models.IntegerField()
+    max_persons = models.IntegerField()
+    booked_out = models.BooleanField()
+
+    def __str__(self):
+        return "Table Number " + self.number
