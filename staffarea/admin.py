9eb5384f5963e4a9c5b5fd1eccff70b2e47fdaed
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 21:51:36 +0200

    Unregister staff area from admin

--- staffarea/admin.py
+++ staffarea/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import StaffArea
-
-@admin.register(StaffArea)
-class PostAdmin(admin.ModelAdmin):
-
-    list_filter = ('booking_number', 'last_name', 'email', 'booking_date')
-    list_display = ['booking_number', 'last_name', 'email', 'booking_date',
-                    'booking_time']
-    search_fields = ['booking_number', 'last_name', 'email', 'booking_date']
