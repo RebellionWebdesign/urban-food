b8db2a08105501d2c46dd8e6592be0398693fb73
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 21:47:59 +0200

    Delete favourite app

--- favourite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Favourite
-
-@admin.register(Favourite)
-class PostAdmin(admin.ModelAdmin):
-
-    list_filter = ('username', 'name')
-    list_display = ['name', 'username', 'favourite']
-    search_fields = ['name', 'username', 'favourite']
