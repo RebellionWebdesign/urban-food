59a4248a5a69d461ba089fb2d1d8f1b2f16f9c32
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 10:55:40 +0200

    Create review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings 
 
-# Create your models here.
+class Review(models.Model):
+
+    BAD = 0
+    MEDIOCRE = 1
+    OKAY = 2
+    GOOD = 3
+    VERY_GOOD = 4
+
+    RATE_CHOICES = (
+        (BAD, 1),
+        (MEDIOCRE, 2),
+        (OKAY, 3)
+        (GOOD, 4)
+        (VERY_GOOD, 5)
+    )
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE,
+                               related_name='review_user')
+    content = models.TextField()
+    rate = models.IntegerField(choices=RATE_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.author
