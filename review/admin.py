7ba1dd24d8b4f2e999025381dffbbc520d8c72c7
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 11:02:32 +0200

    Register review to admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Review)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('author', 'rate', 'created_on')
+    list_display = ['author', 'rate', 'created_on', 'last_modified']
+    search_fields = ['author', 'rate']
+    summernote_fields = ('content')
