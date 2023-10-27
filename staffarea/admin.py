0ba242d5b82df387e122c72f39d90ddcb68fe726
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 11:46:50 +0200

    Register staffarea to admin

--- staffarea/admin.py
+++ staffarea/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import StaffArea
 
-# Register your models here.
+@admin.register(StaffArea)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('booking_number', 'last_name', 'email', 'booking_date')
+    list_display = ['booking_number', 'last_name', 'email', 'table', 'persons',
+                    'booking_date', 'booking_time']
+    search_fields = ['booking_number', 'last_name', 'email', 'table', 'persons',
+                     'booking_date']
