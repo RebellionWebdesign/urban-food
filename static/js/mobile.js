a3568bb0796e918b9d42cc60c904ed3cbde07bde
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 21:12:05 +0200

    Add mobile menu js

---
+++ static/js/mobile.js
@@ -0,0 +1,16 @@
+/*
+This is recycled code from PP1
+*/
+const mobileToggleOpen = document.getElementsByClassName("fa-bars")[0]
+const mobileToggleClose = document.getElementsByClassName("fa-xmark")[0]
+const navBar = document.getElementsByClassName("column-50-mobile")[0]
+
+mobileToggleOpen.addEventListener("click", () => {
+    navBar.classList.remove("closed")
+    navBar.classList.add("open")
+})
+
+mobileToggleClose.addEventListener("click", () => {
+    navBar.classList.remove("open")
+    navBar.classList.add("closed")
+})
