d6a2f9c8eb4785c8952d5abb5ea8dd6036b9db02
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 10:14:44 +0200

    Create booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings 
+from table.models import Table
 
-# Create your models here.
+class Booking(models.Model):
+    number = models.IntegerField()
+    first_name = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                   on_delete=models.CASCADE)
+    last_name = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE)
+    email = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    persons = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    date_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.number + ", " + self.last_name
