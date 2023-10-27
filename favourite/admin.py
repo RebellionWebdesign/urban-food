c4a7507a0fe538c2a4e1243d17b07ed45e4f45bf
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-27 13:36:03 +0200

    Register favourites to admin

--- favourite/admin.py
+++ favourite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Favourite
 
-# Register your models here.
+@admin.register(Favourite)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('username', 'name')
+    list_display = ['name', 'username', 'favourite']
+    search_fields = ['name', 'username', 'favourite']
