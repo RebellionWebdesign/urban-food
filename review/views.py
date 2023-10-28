85c87cdb2cf83b63edab25bd5f58d809e4bab7a9
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-28 11:15:35 +0200

    Add review view for profile

--- review/views.py
+++ review/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Review
 
-# Create your views here.
+class ReviewView(DetailView):
+
+    def get(self, request):
+        reviews = Review.objects.all()
+        context = {
+            "reviews" : reviews
+        }
+        return render(request, 'user/profile.html', context)
