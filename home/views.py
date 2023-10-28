fc5b49c316f4efc0ba0029a042433a22c570127c
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-28 11:14:49 +0200

    Extend view with users

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from review.models import Review
+from user.models import User
+
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home/index.html')
+        reviews = Review.objects.all()
+        users = User.objects.all()
+        context = {
+            "reviews" : reviews,
+            "users" : users
+        }
+        return render(request, 'home/index.html', context)
