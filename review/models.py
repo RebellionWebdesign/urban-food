5b3ad8d01dec2b0390da46385c8b3b0a18b4021f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-28 11:15:02 +0200

    Fix model

--- review/models.py
+++ review/models.py
@@ -3,18 +3,12 @@ from django.conf import settings
 
 class Review(models.Model):
 
-    BAD = 0
-    MEDIOCRE = 1
-    OKAY = 2
-    GOOD = 3
-    VERY_GOOD = 4
-
     RATE_CHOICES = (
-        (BAD, '1'),
-        (MEDIOCRE, '2'),
-        (OKAY, '3'),
-        (GOOD, '4'),
-        (VERY_GOOD, '5'),
+        (0, '1'),
+        (1, '2'),
+        (2, '3'),
+        (3, '4'),
+        (4, '5'),
     )
 
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -26,4 +20,4 @@ class Review(models.Model):
     last_modified = models.DateField(auto_now=True)
 
     def __str__(self):
-        return self.author
+        return str(self.author)
