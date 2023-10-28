c39e0aa1752f753eae4eb75a0bb65215a9160608
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-28 11:15:56 +0200

    Delete unused url

--- urbanfood/urls.py
+++ urbanfood/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls')),
-    path('', include('user.urls')),
     path('accounts/profile/', include('user.urls')),
+
 ]
