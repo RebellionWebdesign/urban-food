692dfa58caa29fc80786a1cda126dee2a590c7bb
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-29 14:30:39 +0100

    Create delete user url

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from .views import BaseView, UserProfile
+from .views import BaseView, UserProfile, DeleteUserView
 from django.urls import path
 
 urlpatterns = [
     path('', BaseView.as_view(), name = 'home'),
-    path('accounts/profile/', UserProfile.as_view(), name='user_profile')
+    path('accounts/profile/', UserProfile.as_view(), name='user_profile'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(),
+         name='user_delete')
 ]
