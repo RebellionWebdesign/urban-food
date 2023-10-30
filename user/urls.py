b14212bc9eba3cf1287ffb2986f93b3b0547e17f
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-30 12:14:33 +0100

    Format document

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from .views import BaseView, UserProfile, DeleteUserView, ChangeUserView
 from django.urls import path
 
 urlpatterns = [
-    path('', BaseView.as_view(), name = 'home'),
+    path('', BaseView.as_view(), name='home'),
     path('accounts/profile/', UserProfile.as_view(), name='user_profile'),
     path('accounts/profile/delete/', DeleteUserView.as_view(),
          name='user_delete'),
