ede344d3247c6b6ae9b95d6971586d0ada02fd33
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-30 09:42:14 +0100

    Add url to user change page

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
-from .views import BaseView, UserProfile, DeleteUserView
+from .views import BaseView, UserProfile, DeleteUserView, ChangeUserView
 from django.urls import path
 
 urlpatterns = [
     path('', BaseView.as_view(), name = 'home'),
     path('accounts/profile/', UserProfile.as_view(), name='user_profile'),
     path('accounts/profile/delete/', DeleteUserView.as_view(),
-         name='user_delete')
+         name='user_delete'),
+    path('accounts/profile/update/', ChangeUserView.as_view(), name='user_change')
 ]
