66f469df189722507a6ec31f94ee1253dc1224fd
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-30 12:27:28 +0100

    Add user review view

--- review/views.py
+++ review/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, View
 from .models import Review
 
 class ReviewView(DetailView):
@@ -10,3 +11,12 @@ class ReviewView(DetailView):
             "reviews" : reviews
         }
         return render(request, 'user/profile.html', context)
+
+class UserProfileReview(LoginRequiredMixin, View):
+    
+    def get(self, request):
+        user_reviews = Review.objects.all()
+        context = {
+            "user_reviews":user_reviews
+        }
+        return render(request, 'review/my_reviews.html', context)
