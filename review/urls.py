c24c049ab9682ba403dc5f8f8051bf679af8a05e
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-31 11:48:08 +0100

    Create new user review url

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,7 @@
-from .views import UserProfileReview
+from .views import UserProfileReview, NewReview
 from django.urls import path
 
 urlpatterns = [
     path('', UserProfileReview.as_view(), name = 'user_reviews'),
+    path('new_review/', NewReview.as_view(), name='new_review')
 ]
