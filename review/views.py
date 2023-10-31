6651dcf01ab651e7e4e741e6fa5660a7f3e2b6ad
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-31 11:48:25 +0100

    Fix user review

--- review/views.py
+++ review/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView, View
 from .models import Review
+from .forms import NewReviewForm
 
 class ReviewView(DetailView):
 
@@ -12,11 +13,36 @@ class ReviewView(DetailView):
         }
         return render(request, 'user/profile.html', context)
 
-class UserProfileReview(LoginRequiredMixin, View):
+class UserProfileReview(LoginRequiredMixin, DetailView):
     
     def get(self, request):
-        user_reviews = Review.objects.all()
+        reviews = Review.objects.filter(author=request.user).order_by('-created_on')
         context = {
-            "user_reviews":user_reviews
+            "reviews":reviews
         }
         return render(request, 'review/my_reviews.html', context)
+    
+    def get_object(self):
+        return self.request.user
+
+class NewReview(LoginRequiredMixin, View):
+
+    def get(self, request):
+        review_form = NewReviewForm(request.POST, instance=request.user)
+
+        return render(request, 'review/new_review.html', {'form': review_form})
+    
+    def post(self, request):
+
+        if request.method == 'POST':
+            review_form = NewReviewForm(data=request.POST, instance=request.user)
+            if review_form.is_valid():
+                author = request.user
+                review = review_form.cleaned_data['content']
+                rate = review_form.cleaned_data['rate']
+                new_review = Review(author = author,
+                                    content = review,
+                                    rate = rate)
+                new_review.save()
+
+        return redirect('user_reviews')
