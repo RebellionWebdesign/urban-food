231513154bd1acec05b6519ef1dd4a41f03732dd
Daniel Stauffer <info@rebellion-webdesign.com>
2023-10-31 15:04:07 +0100

    Add DeleteReview view

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,8 @@
-from .views import UserProfileReview, NewReview
+from .views import UserProfileReview, NewReview, DeleteReview
 from django.urls import path
 
 urlpatterns = [
     path('', UserProfileReview.as_view(), name = 'user_reviews'),
-    path('new_review/', NewReview.as_view(), name='new_review')
+    path('new_review/', NewReview.as_view(), name='new_review'),
+    path('delete_review/', DeleteReview.as_view(), name='delete_review'),
 ]
