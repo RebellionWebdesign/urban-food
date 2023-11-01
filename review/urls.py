fe68ba3eeee29ea551cf2f55a9d1970ae6a7301b
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-01 07:09:36 +0100

    Fix deletion url

--- review/urls.py
+++ review/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', UserProfileReview.as_view(), name = 'user_reviews'),
     path('new_review/', NewReview.as_view(), name='new_review'),
-    path('delete_review/', DeleteReview.as_view(), name='delete_review'),
+    path('delete_review/<int:pk>', DeleteReview.as_view(), name='delete_review'),
 ]
