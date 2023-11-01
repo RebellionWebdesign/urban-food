1f0a2de637bffe500f9dd03d8383390c406473db
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-01 10:38:30 +0100

    Add update_review url

--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,9 @@
-from .views import UserProfileReview, NewReview, DeleteReview
+from .views import UserProfileReview, NewReview, DeleteReview, UpdateReview
 from django.urls import path
 
 urlpatterns = [
     path('', UserProfileReview.as_view(), name = 'user_reviews'),
     path('new_review/', NewReview.as_view(), name='new_review'),
     path('delete_review/<int:pk>', DeleteReview.as_view(), name='delete_review'),
+    path('update_review/<int:pk>', UpdateReview.as_view(), name='update_review'),
 ]
