85e04963cb961774b523b36693420bd674cb307c
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-02 13:22:05 +0100

    Fix profile urls

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', BaseView.as_view(), name='home'),
-    path('accounts/profile/', UserProfile.as_view(), name='user_profile'),
-    path('accounts/profile/delete/', DeleteUserView.as_view(),
-         name='user_delete'),
-    path('accounts/profile/update/', ChangeUserView.as_view(),
-         name='user_change'),
+    path('accounts/profile/', UserProfile.as_view(),name='user_profile'),
+    path('accounts/profile/edit/', ChangeUserView.as_view(),name='user_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(),name='user_delete'),
 ]
