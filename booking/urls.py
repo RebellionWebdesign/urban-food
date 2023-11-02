f1b09d79643655e51bda463db1017add92f92e74
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-02 13:19:38 +0100

    Fix urls

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,7 @@ from .views import BookingView, NewBooking, DeleteBooking
 from django.urls import path
 
 urlpatterns = [
-    path('account/bookings/', BookingView.as_view(), name='user_bookings'),
-    path('account/bookings/new_booking/', NewBooking.as_view(), name='new_booking'),
-    path('account/bookings/delete_booking/<int:pk>', DeleteBooking.as_view(), name='delete_booking'),
+    path('accounts/profile/bookings/', BookingView.as_view(), name='user_bookings'),
+    path('accounts/profile/bookings/new_booking/', NewBooking.as_view(), name='new_booking'),
+    path('accounts/profile/bookings/delete_booking/<int:pk>', DeleteBooking.as_view(), name='delete_booking'),
 ]
