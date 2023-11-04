b6cf7ea4c82e1c4b0cdb8b8d36f12a013b872e16
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-04 13:05:59 +0100

    Format document

--- booking/forms.py
+++ booking/forms.py
@@ -1,19 +1,18 @@
 from django.forms import ModelForm, DateInput, TimeInput
 from .models import Booking
 
+
 class NewBookingForm(ModelForm):
     """
     This class presents a booking form to the user. The fields like number,
     date_created and first_name/last_name are omitted because they are auto
     generated.
     """
-
-
     class Meta:
         model = Booking
         fields = ['date', 'time']
         widgets = {
-            'date': DateInput(attrs={'class':'datepicker', 'type':'date'}),
-            'time': TimeInput(attrs={'class':'timepicker', 'min':'12:00',
-                                     'max':'23:00', 'type':'time'})
+            'date': DateInput(attrs={'class': 'datepicker', 'type': 'date'}),
+            'time': TimeInput(attrs={'class': 'timepicker', 'min': '12:00',
+                                     'max': '23:00', 'type': 'time'})
         }
