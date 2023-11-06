0a7c771b140cce672dcd950531369fa694ec7800
Daniel Stauffer <info@rebellion-webdesign.com>
2023-11-06 08:38:35 +0100

    Format document

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,24 @@
 /*
 This is recycled code from PP1
 */
-const mobileToggleOpen = document.getElementsByClassName("fa-bars")[0]
-const mobileToggleClose = document.getElementsByClassName("fa-xmark")[0]
-const navBar = document.getElementsByClassName("column-50-mobile")[0]
+const mobileToggleOpen = document.getElementsByClassName("fa-bars")[0];
+const mobileToggleClose = document.getElementsByClassName("fa-xmark")[0];
+const navBar = document.getElementsByClassName("column-50-mobile")[0];
 
 mobileToggleOpen.addEventListener("click", () => {
-    navBar.classList.remove("closed")
-    navBar.classList.add("open")
-})
+    navBar.classList.remove("closed");
+  navBar.classList.add("open");
+});
 
 mobileToggleClose.addEventListener("click", () => {
-    navBar.classList.remove("open")
-    navBar.classList.add("closed")
-})
+    navBar.classList.remove("open");
+    navBar.classList.add("closed");
+});
 
 /* This will remove the displayed messages after a certain time */
 setTimeout(() => {
-    const message = document.getElementById("messages")
+    const message = document.getElementById("messages");
     if ( message ) {
-        message.style.display = 'none'
+        message.style.display = 'none';
     }
-}, 3000)
+}, 3000);
